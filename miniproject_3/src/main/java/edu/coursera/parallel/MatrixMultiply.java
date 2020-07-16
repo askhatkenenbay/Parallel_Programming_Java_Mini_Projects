8b89e6748f5a5c0e28a50d1d8d5d1fb2c249f9d8
Askhat <askhat.kenenbay@nu.edu.kz>
2020-07-16 22:58:48 +0600

    commit

---
+++ miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
@@ -0,0 +1,118 @@
+package edu.coursera.parallel;
+
+import edu.rice.pcdp.ProcedureInt1D;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static edu.rice.pcdp.PCDP.forseq2d;
+import static edu.rice.pcdp.PCDP.forall;
+/**
+ * Wrapper class for implementing matrix multiply efficiently in parallel.
+ */
+public final class MatrixMultiply {
+    /**
+     * Default constructor.
+     */
+    private MatrixMultiply() {
+    }
+
+    /**
+     * Perform a two-dimensional matrix multiply (A x B = C) sequentially.
+     *
+     * @param A An input matrix with dimensions NxN
+     * @param B An input matrix with dimensions NxN
+     * @param C The output matrix
+     * @param N Size of each dimension of the input matrices
+     */
+    public static void seqMatrixMultiply(final double[][] A, final double[][] B,
+            final double[][] C, final int N) {
+        forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
+            C[i][j] = 0.0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        });
+    }
+
+    /**
+     * Perform a two-dimensional matrix multiply (A x B = C) in parallel.
+     *
+     * @param A An input matrix with dimensions NxN
+     * @param B An input matrix with dimensions NxN
+     * @param C The output matrix
+     * @param N Size of each dimension of the input matrices
+     */
+    public static void parMatrixMultiply(final double[][] A, final double[][] B,
+            final double[][] C, final int N) {
+        /*
+         * TODO Parallelize this outermost two-dimension sequential loop to
+         * achieve performance improvement.
+         */
+        /*
+        forall(0, N - 1, 0, N - 1, (i, j) -> {
+            C[i][j] = 0.0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        });*/
+        ParallelThreadsCreator.multiply(A,B,C);
+    }
+}
+class RowMultiplyWorker implements Runnable {
+
+    private final double[][] result;
+    private double[][] matrix1;
+    private double[][] matrix2;
+    private final int row;
+
+    public RowMultiplyWorker(double[][] result, double[][] matrix1, double[][] matrix2, int row) {
+        this.result = result;
+        this.matrix1 = matrix1;
+        this.matrix2 = matrix2;
+        this.row = row;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < matrix2[0].length; i++) {
+            result[row][i] = 0;
+            for (int j = 0; j < matrix1[row].length; j++) {
+                result[row][i] += matrix1[row][j] * matrix2[j][i];
+
+            }
+
+        }
+
+    }
+
+}
+class ParallelThreadsCreator {
+
+    // creating 10 threads and waiting for them to complete then again repeat steps.
+    public static void multiply(double[][] matrix1, double[][] matrix2, double[][] result) {
+        List threads = new ArrayList<>();
+        int rows1 = matrix1.length;
+        for (int i = 0; i < rows1; i++) {
+            RowMultiplyWorker task = new RowMultiplyWorker(result, matrix1, matrix2, i);
+            Thread thread = new Thread(task);
+            thread.start();
+            threads.add(thread);
+            if (threads.size() % 20 == 0) {
+                waitForThreads(threads);
+            }
+        }
+    }
+
+    private static void waitForThreads(List<Thread> threads) {
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        threads.clear();
+    }
+}
